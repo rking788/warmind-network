fbc8abd48cb055d6b67414c2002034f44eb5f730
Rob King <rking188@gmail.com>
2019-01-03 18:31:11 -0500

    Refactor bungie methods to return a speech string instead of an EchoResponse
    This way the bungie package doesn't need to know anything about the skillserver package and it will be more flexible when it comes to working with other assistants/interfaces not just Echo

--- alexa/alexa.go
+++ alexa/alexa.go
@@ -4,20 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/getsentry/raven-go"
-
 	"github.com/kpango/glg"
+	"github.com/rking788/go-alexa/skillserver"
 	"github.com/rking788/warmind-network/bungie"
 	"github.com/rking788/warmind-network/charlemagne"
 	"github.com/rking788/warmind-network/db"
 	"github.com/rking788/warmind-network/trials"
-
-	"strings"
-
-	"github.com/garyburd/redigo/redis"
-	"github.com/rking788/go-alexa/skillserver"
 )
 
 // Session is responsible for storing information related to a specific skill invocation.
@@ -158,27 +155,31 @@ func HelpPrompt(echoRequest *skillserver.EchoRequest) (response *skillserver.Ech
 
 // CountItem calls the Bungie API to see count the number of Items on all characters and
 // in the vault.
-func CountItem(echoRequest *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func CountItem(echoRequest *skillserver.EchoRequest) *skillserver.EchoResponse {
 
+	response := skillserver.NewEchoResponse()
 	accessToken := echoRequest.Session.User.AccessToken
 	item, _ := echoRequest.GetSlotValue("Item")
 	lowerItem := strings.ToLower(item)
-	response, err := bungie.CountItem(lowerItem, accessToken)
-	if err != nil {
+	speech, err := bungie.CountItem(lowerItem, accessToken)
+	if err != nil || speech == "" {
 		raven.CaptureError(err, nil)
 		glg.Errorf("Error counting the number of items: %s", err.Error())
 		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, an error occurred counting that item.")
 	}
 
-	return
+	response.OutputSpeech(speech)
+
+	return response
 }
 
 // TransferItem will attempt to transfer either a specific quantity or all of a
 // specific item to a specified character. The item name and destination are the
 // required fields. The quantity and source are optional.
-func TransferItem(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func TransferItem(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
+	response := skillserver.NewEchoResponse()
 	accessToken := request.Session.User.AccessToken
 	countStr, _ := request.GetSlotValue("Count")
 	count := -1
@@ -187,13 +188,13 @@ func TransferItem(request *skillserver.EchoRequest) (response *skillserver.EchoR
 		if ok != nil {
 			response = skillserver.NewEchoResponse()
 			response.OutputSpeech("Sorry Guardian, I didn't understand the number you asked to be transferred. Do not specify a quantity if you want all to be transferred.")
-			return
+			return response
 		}
 
 		if tempCount <= 0 {
 			output := fmt.Sprintf("Sorry Guardian, you need to specify a positive, non-zero number to be transferred, not %d", tempCount)
 			response.OutputSpeech(output)
-			return
+			return response
 		}
 
 		count = tempCount
@@ -203,82 +204,85 @@ func TransferItem(request *skillserver.EchoRequest) (response *skillserver.EchoR
 	sourceClass, _ := request.GetSlotValue("Source")
 	destinationClass, _ := request.GetSlotValue("Destination")
 	if destinationClass == "" {
-		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, you must specify a destination for the items to be transferred.")
-		return
+		return response
 	}
 
 	glg.Infof("Transferring %d of your %s from your %s to your %s", count, strings.ToLower(item), strings.ToLower(sourceClass), strings.ToLower(destinationClass))
-	response, err := bungie.TransferItem(strings.ToLower(item), accessToken, strings.ToLower(sourceClass), strings.ToLower(destinationClass), count)
+	speech, err := bungie.TransferItem(strings.ToLower(item), accessToken, strings.ToLower(sourceClass), strings.ToLower(destinationClass), count)
 	if err != nil {
 		raven.CaptureError(err, nil)
-		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, an error occurred trying to transfer that item.")
-		return
+		return response
 	}
 
-	return
+	response.OutputSpeech(speech)
+	return response
 }
 
 // MaxPower will equip the loadout on the current character that provides the maximum
 // amount of power.
-func MaxPower(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func MaxPower(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
+	response := skillserver.NewEchoResponse()
 	accessToken := request.Session.User.AccessToken
-	response, err := bungie.EquipMaxLightGear(accessToken)
+	speech, err := bungie.EquipMaxLightGear(accessToken)
 	if err != nil {
 		raven.CaptureError(err, nil)
 		glg.Errorf("Error occurred equipping max power: %s", err.Error())
-		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, an error occurred equipping your max power gear.")
 	}
 
-	return
+	response.OutputSpeech(speech)
+	return response
 }
 
 // RandomGear will equip random gear for each of the current characters gear slots (excluding
 // things like ghost and class items). It is possible to randomize only weapons or weapons
 // and armor.
-func RandomGear(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func RandomGear(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
+	response := skillserver.NewEchoResponse()
 	accessToken := request.Session.User.AccessToken
-	response, err := bungie.RandomizeLoadout(accessToken)
+	speech, err := bungie.RandomizeLoadout(accessToken)
 	if err != nil {
 		raven.CaptureError(err, nil)
 		glg.Errorf("Error occurred equipping random gear: %s", err.Error())
-		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, an error occurred equipping random gear.")
 	}
 
-	return
+	response.OutputSpeech(speech)
+	return response
 }
 
 // UnloadEngrams will take all engrams on all of the current user's characters and
 // transfer them all to the vault to allow the player to continue farming.
-func UnloadEngrams(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func UnloadEngrams(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
+	response := skillserver.NewEchoResponse()
 	accessToken := request.Session.User.AccessToken
-	response, err := bungie.UnloadEngrams(accessToken)
+	speech, err := bungie.UnloadEngrams(accessToken)
 	if err != nil {
 		raven.CaptureError(err, nil)
 		glg.Errorf("Error occurred unloading engrams: %s", err.Error())
-		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, an error occurred moving your engrams.")
+		return response
 	}
 
-	return
+	response.OutputSpeech(speech)
+	return response
 }
 
 // DestinyJoke will return the desired text for a random joke from the database.
-func DestinyJoke(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func DestinyJoke(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
-	response = skillserver.NewEchoResponse()
+	response := skillserver.NewEchoResponse()
 	setup, punchline, err := db.GetRandomJoke()
 	if err != nil {
 		raven.CaptureError(err, nil)
 		glg.Errorf("Error loading joke from DB: %s", err.Error())
 		response.OutputSpeech("Sorry Guardian, I was unable to load a joke right now.")
-		return
+		return response
 	}
 
 	builder := skillserver.NewSSMLTextBuilder()
@@ -287,28 +291,28 @@ func DestinyJoke(request *skillserver.EchoRequest) (response *skillserver.EchoRe
 		AppendPlainSpeech(punchline)
 	response.OutputSpeechSSML(builder.Build())
 
-	return
+	return response
 }
 
 // CreateLoadout will determine the user's current character and create a new loadout based on
 // their currently equipped items. This loadout is then serialized and persisted to a
 // persistent store.
-func CreateLoadout(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func CreateLoadout(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
-	response = skillserver.NewEchoResponse()
+	response := skillserver.NewEchoResponse()
 
 	glg.Debugf("Found dialog state = %s", request.GetDialogState())
 	if request.GetDialogState() != skillserver.DialogCompleted {
 		// The user still needs to provide a name for the new loadout to be created
 		response.DialogDelegate(nil)
-		return
+		return response
 	}
 
 	glg.Debugf("Found intent confirmation status = %s", request.GetIntentConfirmationStatus())
 	intentConfirmation := request.GetIntentConfirmationStatus()
 	if intentConfirmation == skillserver.ConfirmationDenied {
 		// The user does NOT want to overwrite the existing loadout with the same name
-		return
+		return response
 	}
 
 	accessToken := request.Session.User.AccessToken
@@ -317,9 +321,17 @@ func CreateLoadout(request *skillserver.EchoRequest) (response *skillserver.Echo
 		response.OutputSpeech("Sorry Guardian, you must specify a name for the loadout being saved.")
 	}
 
+	exists, _ := bungie.DoesLoadoutExist(accessToken, loadoutName)
+	if exists && intentConfirmation == "" {
+		// Ask the user if they wish to overwrite this loadout
+		response.ConfirmIntent("CreateLoadout", nil).
+			OutputSpeech(fmt.Sprintf("You already have a loadout named %s, would you like to overwrite it?", loadoutName))
+		return response
+	}
+
 	var err error
-	response, err = bungie.CreateLoadoutForCurrentCharacter(accessToken, loadoutName,
-		intentConfirmation == "CONFIRMED")
+	shouldOverwrite := intentConfirmation == skillserver.ConfirmationConfirmed
+	speech, err := bungie.CreateLoadoutForCurrentCharacter(accessToken, loadoutName, shouldOverwrite)
 
 	if err != nil {
 		raven.CaptureError(err, nil)
@@ -327,45 +339,52 @@ func CreateLoadout(request *skillserver.EchoRequest) (response *skillserver.Echo
 		response.OutputSpeech("Sorry Guardian, an error occurred saving your loadout.")
 	}
 
-	return
+	response.OutputSpeech(speech)
+
+	return response
 }
 
 // EquipNamedLoadout will take the name of a loadout and try to retrieve it from the database
 // and equip it on the user's currently active character.
-func EquipNamedLoadout(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func EquipNamedLoadout(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
-	response = skillserver.NewEchoResponse()
+	response := skillserver.NewEchoResponse()
 	accessToken := request.Session.User.AccessToken
 	loadoutName, _ := request.GetSlotValue("Name")
 	if loadoutName == "" {
 		response.OutputSpeech("Sorry Guardian, you must specify a name for the loadout being equipped.")
+		return response
 	}
 
-	response, err := bungie.EquipNamedLoadout(accessToken, loadoutName)
+	speech, err := bungie.EquipNamedLoadout(accessToken, loadoutName)
 
 	if err != nil {
 		raven.CaptureError(err, nil)
 		glg.Errorf("Error occurred equipping loadout: %s", err.Error())
 		response.OutputSpeech("Sorry Guardian, an error occurred equipping your loadout.")
+		return response
 	}
 
-	return
+	response.OutputSpeech(speech)
+	return response
 }
 
 // ListLoadouts provides a speech response back to the user that lists the names
 // of the currently saved loadouts that are found in the DB.
-func ListLoadouts(request *skillserver.EchoRequest) (response *skillserver.EchoResponse) {
+func ListLoadouts(request *skillserver.EchoRequest) *skillserver.EchoResponse {
 
+	response := skillserver.NewEchoResponse()
 	accessToken := request.Session.User.AccessToken
-	response, err := bungie.GetLoadoutNames(accessToken)
+	speech, err := bungie.GetLoadoutNames(accessToken)
 	if err != nil {
 		raven.CaptureError(err, nil)
 		response = skillserver.NewEchoResponse()
 		response.OutputSpeech("Sorry Guardian, there was an error getting your loadout names, please try again later")
-		return
+		return response
 	}
 
-	return
+	response.OutputSpeech(speech)
+	return response
 }
 
 // GetCurrentRank will summarize the player's current ranking in various activities. The activity
@@ -376,7 +395,7 @@ func GetCurrentRank(request *skillserver.EchoRequest) *skillserver.EchoResponse 
 	accessToken := request.Session.User.AccessToken
 	progression, _ := request.GetSlotValue("Progression")
 
-	var response *skillserver.EchoResponse
+	var speech string
 	var err error
 	switch strings.ToLower(progression) {
 	case "":
@@ -390,24 +409,25 @@ func GetCurrentRank(request *skillserver.EchoRequest) *skillserver.EchoResponse 
 	case "competitive":
 		fallthrough
 	case "crucible":
-		response, err = bungie.GetCurrentCrucibleRanking(accessToken)
+		speech, err = bungie.GetCurrentCrucibleRanking(accessToken)
 	case "infamy":
 		fallthrough
 	case "gambit":
-		response, err = bungie.GetCurrentGambitRanking(accessToken)
+		speech, err = bungie.GetCurrentGambitRanking(accessToken)
 	default:
-		response = skillserver.NewEchoResponse()
-		response.OutputSpeech("Sorry Guardian, I'm not sure how to get information about a " + progression + " rank")
+		speech = "Sorry Guardian, I'm not sure how to get information about a " + progression + " rank"
 	}
 
-	if response == nil || err != nil {
+	response := skillserver.NewEchoResponse()
+	if speech == "" || err != nil {
 		raven.CaptureError(err, nil)
-		response = skillserver.NewEchoResponse()
-		response.OutputSpeech("Sorry Guardian, there was an error getting your current " +
-			progression + " ranking, please try again later.")
+		outputStr := "Sorry Guardian, there was an error getting your current " +
+			progression + " ranking, please try again later."
+		response.OutputSpeech(outputStr)
 		return response
 	}
 
+	response.OutputSpeech(speech)
 	return response
 }
 
